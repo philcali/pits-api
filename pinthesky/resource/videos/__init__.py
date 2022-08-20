cf0dfbf8a82f74a792e4fbafd358dc6086d885dc
Philip Cali <philip.cali@gmail.com>
2022-08-19 20:45:32 -0400

    fix: wrong field name

--- pinthesky/resource/videos/__init__.py
+++ pinthesky/resource/videos/__init__.py
@@ -15,25 +15,25 @@ def list_motion_videos(motion_videos, first_index):
     next_token = request.queryparams.get('nextToken', None)
     start_time = request.queryparams.get('startTime', None)
     end_time = request.queryparams.get('endTime', None)
-    filters = []
+    sort_filters = []
     if start_time is not None and end_time is not None:
         start_timestamp = datetime.fromisoformat(start_time).timestamp()
         end_timestamp = datetime.fromisoformat(end_time).timestamp()
-        filters.append(SortFilter(
+        sort_filters.append(SortFilter(
             name='createTime',
             method='between',
             values=[floor(start_timestamp), floor(end_timestamp)]
         ))
     elif start_time is not None:
         start_timestamp = datetime.fromisoformat(start_time).timestamp()
-        filters.append(SortFilter(
+        sort_filters.append(SortFilter(
             name='createTime',
             method='lt',
             values=[floor(start_timestamp)]
         ))
     elif end_time is not None:
         end_timestamp = datetime.fromisoformat(end_time).timestamp()
-        filters.append(SortFilter(
+        sort_filters.append(SortFilter(
             name='createTime',
             method='gt',
             values=[floor(end_timestamp)]
@@ -44,7 +44,7 @@ def list_motion_videos(motion_videos, first_index):
         params=QueryParams(
             limit=limit,
             next_token=next_token,
-            filters=filters))
+            sort_filters=sort_filters))
     return {
         'items': page.items,
         'nextToken': page.next_token
