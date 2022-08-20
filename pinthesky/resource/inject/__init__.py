0ddb77c5928aff784e29ed96537e56090d948039
Philip Cali <philip.cali@gmail.com>
2022-08-19 20:29:48 -0400

    feat: video endpoint

--- pinthesky/resource/inject/__init__.py
+++ pinthesky/resource/inject/__init__.py
@@ -6,5 +6,6 @@ TABLE_NAME = os.getenv('TABLE_NAME')
 
 ddb = boto3.resource('dynamodb')
 app_context.inject('table', ddb.Table(TABLE_NAME))
+app_context.inject('first_index', os.getenv('INDEX_NAME_1'))
 app_context.inject('bucket_name', os.getenv('BUCKET_NAME'))
 app_context.inject('image_prefix', os.getenv('IMAGE_PREFIX'))
