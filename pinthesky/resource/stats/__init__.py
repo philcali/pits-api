1b6f37571add89ac55e73bf5bf26668e0b649dc7
Philip Cali <philip.cali@gmail.com>
2022-09-13 17:42:37 -0400

    fix: batch stats for things

--- pinthesky/resource/stats/__init__.py
+++ pinthesky/resource/stats/__init__.py
@@ -1,19 +1,39 @@
-from pinthesky.database import DeviceHealth
+import re
+from pinthesky.database import DeviceHealth, Repository
 from pinthesky.resource import api
-from pinthesky.globals import app_context, request
-from pinthesky.resource.helpers import create_query_params
+from pinthesky.globals import app_context, request, response
+from pinthesky.resource.helpers import create_query_params, get_limit
 
 app_context.inject('stats_data', DeviceHealth())
 
 
-@api.route('/stats', methods=['GET'])
+@api.route('/stats')
 def list_latest(stats_data):
-    page = stats_data.items(
-        request.account_id(),
-        'latest',
-        params=create_query_params(request)
-    )
+    thing_names = request.queryparams.get('thingName', None)
+    if thing_names is None:
+        page = stats_data.items(
+            request.account_id(),
+            'latest',
+            params=create_query_params(request)
+        )
+        return {
+            'items': page.items,
+            'nextToken': page.next_token
+        }
+    item_ids = re.split('\\s*,\\s*', thing_names)
+    limit = get_limit(request)
+    if len(item_ids) > limit:
+        response.status_code = 400
+        return {
+            'message': f'Provided {len(item_ids)} is more than {limit}.'
+        }
     return {
-        'items': page.items,
-        'nextToken': page.next_token
+        'items': Repository.batch_read(
+            request.account_id(),
+            'latest',
+            reads=[
+                {'id': item_id, 'repository': stats_data}
+                for item_id in item_ids
+            ]
+        )
     }
