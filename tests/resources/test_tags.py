ec7ff656d1bf59206dcbe0d6ff3c492166b5b668
Philip Cali <philip.cali@gmail.com>
2022-11-02 21:13:55 -0400

    fix: tag delete loops forever

--- tests/resources/test_tags.py
+++ tests/resources/test_tags.py
@@ -19,6 +19,8 @@ def test_tag_crud_workflow(tags, videos):
         'name': 'Favorites'
     })
 
+    assert tags(method="POST", body={}).code == 401
+
     assert created_tag.code == 200
     tag = created_tag.body
 
@@ -47,7 +49,36 @@ def test_tag_crud_workflow(tags, videos):
     assert tags('/Favorites/videos', method="POST", body={
         'videos': [video]
     }).code == 200
+    assert videos(
+        f'/{video["motionVideo"]}/cameras/PitsCamera1'
+    ).body == video
+    assert videos(
+        f'/{video["motionVideo"]}/cameras/PitsCamera1',
+        method="DELETE").code == 200
+    assert videos(
+        f'/{video["motionVideo"]}/cameras/PitsCamera1'
+    ).code == 404
+
+    video = motion_videos.create(
+        tags.account_id(),
+        'PitsCamera1',
+        item={
+            'GS1-PK': motion_videos.make_hash_key(tags.account_id()),
+            'motionVideo': title,
+            'thingName': 'PitsCamera1',
+            'duration': 30,
+            'expiresIn': floor(time())
+        })
+    assert tags('/Favorites/videos', method="POST", body={
+        'videos': [video]
+    }).code == 200
 
     video['id'] = hashed_video(video['motionVideo'], 'PitsCamera1')
     assert tags('/Favorites/videos').body['items'][0] == video
     assert tags(f'/Favorites/videos/{title}', method="DELETE").code == 200
+
+    assert tags('/Favorites/videos', method="POST", body={
+        'videos': [video]
+    }).code == 200
+
+    assert tags('/Favorites', method="DELETE").code == 200
