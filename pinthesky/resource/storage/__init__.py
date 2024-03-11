0d2664b0d725b1253471742f1fb04a861861d54e
Philip Cali <philip.cali@gmail.com>
2024-03-11 18:35:39 -0400

    chore: fix flake8

--- pinthesky/resource/storage/__init__.py
+++ pinthesky/resource/storage/__init__.py
@@ -8,10 +8,8 @@ app_context.inject('s3', boto3.client('s3'))
 
 
 @api.route('/storage/info')
-def get_storage_info(
-    bucket_name,
-    video_prefix,
-    image_prefix):
+def get_storage_info(bucket_name, video_prefix, image_prefix):
+
     return {
         'bucketName': bucket_name,
         'videoPrefix': video_prefix,
