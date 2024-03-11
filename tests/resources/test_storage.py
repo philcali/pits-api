3ca3c082264443df364ba475f3f008078ccf307e
Philip Cali <philip.cali@gmail.com>
2024-03-11 18:33:58 -0400

    feat: allow the api to return where storage data comes from

---
+++ tests/resources/test_storage.py
@@ -0,0 +1,10 @@
+
+def test_storage(storage):
+
+    info = storage('/info')
+    assert info.code == 200
+    assert info.body == {
+        'bucketName': 'NOT_FOUND',
+        'videoPrefix': 'videos',
+        'imagePrefix': 'images',
+    }
