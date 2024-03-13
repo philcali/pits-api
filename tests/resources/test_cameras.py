e00ff2e73093c1cd21cf38f9dc0c316f2e2913bd
Philip Cali <philip.cali@gmail.com>
2024-03-13 18:08:25 -0400

    feat: be able to send reported and desired

--- tests/resources/test_cameras.py
+++ tests/resources/test_cameras.py
@@ -110,6 +110,15 @@ def test_camera_crud_workflow(cameras):
                         'cloudwatch': {
                             'enabled': True
                         }
+                    },
+                    'desired': {
+                        'camera': {
+                            'camera_field1': 2,
+                            'camera_field2': 2
+                        },
+                        'cloudwatch': {
+                            'enabled': False
+                        },
                     }
                 }
             }))
@@ -134,6 +143,33 @@ def test_camera_crud_workflow(cameras):
         },
         'cloudwatch': {
             'enabled': True
+        }
+    }
+    configuration = cameras(
+        f'/{cam1["thingName"]}/configuration',
+        query_params={
+            'document': 'camera,cloudwatch',
+            'state': 'desired,reported,creed',
+        })
+    assert configuration.code == 200
+    assert configuration.body == {
+        'desired': {
+            'camera': {
+                'camera_field1': 2,
+                'camera_field2': 2
+            },
+            'cloudwatch': {
+                'enabled': False
+            }
+        },
+        'reported': {
+            'camera': {
+                'camera_field1': 1,
+                'camera_field2': 2
+            },
+            'cloudwatch': {
+                'enabled': True
+            }
         }
     }
     assert cameras('/PitsCamera2/configuration').code == 404
