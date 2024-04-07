0ffbd52e5b5106434b53b40f6dc35d8c087080f2
Philip Cali <philip.cali@gmail.com>
2024-04-07 16:46:44 -0400

    chore: update workflow

--- pinthesky/resource/cameras/__init__.py
+++ pinthesky/resource/cameras/__init__.py
@@ -4,7 +4,7 @@ from uuid import uuid4
 from botocore.exceptions import ClientError
 from pinthesky import api
 from pinthesky.conversion import timestamp_to_motion
-from pinthesky.database import Cameras, CamerasToGroups 
+from pinthesky.database import Cameras, CamerasToGroups
 from ophis.database import ConflictException, Repository, QueryParams
 from ophis.globals import app_context, request, response
 from pinthesky.resource.helpers import create_query_params, get_limit
